c61f0a1a9de0e95449331ee4a47fe21ec78dab18
Ronald George <ronaldsamgeorge@gmail.com>
2022-04-25 13:56:02 +0530

    Add a div with an h1 and a p content in the DOM, exercise #3

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ h3Blue.style.color = 'blue';
 h3Blue.textContent = "I'm a blue h3!"
 
 container.appendChild(h3Blue);
+
+
+/* a <div> with a black border and pink background color with the following elements inside of it:
+a.  another <h1> that says “I’m in a div”
+b.  a <p> that says “ME TOO!” */
+
+const newContainer = document.createElement('div');
+newContainer.style.cssText = 'background-color : pink, border: 2px solid black';
+
+const h1Div = document.createElement('h1');
+h1Div.textContent="I'm in a div"
+
+const textInDiv = document.createElement('p');
+textInDiv.textContent = 'ME TOO!';
+
+newContainer.appendChild(h1Div);
+newContainer.appendChild(textInDiv);
+
+console.log(newContainer);
+container.appendChild(newContainer);
