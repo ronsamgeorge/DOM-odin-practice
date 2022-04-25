282ea3a813a6d6e5cb1f8b4988bd787cfb718649
Ronald George <ronaldsamgeorge@gmail.com>
2022-04-25 13:40:12 +0530

    Add blue h3 text, exercise #2

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 
-//a <p> with red text that says “Hey I’m red!”
+
 
 console.log("working");
 
-const container = document.querySelector("#container");
+const container = document.querySelector('#container');
 
-const redText = document.createElement("p");
+//a <p> with red text that says “Hey I’m red!”
+
+const redText = document.createElement('p');
 redText.style.color = 'red';
 redText.textContent = "Hey I'm red!";
 
 container.appendChild(redText);
+
+
+// #2 add an <h3> with blue text that says “I’m a blue h3!”
+
+const h3Blue = document.createElement('h3');
+h3Blue.style.color = 'blue';
+h3Blue.textContent = "I'm a blue h3!"
+
+container.appendChild(h3Blue);
